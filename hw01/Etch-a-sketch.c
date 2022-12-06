a94953b81c304d7aac0c993f38f8a47ddc3a2318
Wenzi <qianw1@rose-hulman.edu>
2022-12-06 00:00:58 -0500

    Complete homework

--- hw01/Etch-a-sketch.c
+++ hw01/Etch-a-sketch.c
@@ -1,5 +1,13 @@
+/**********************/
+/* Author: Wenzi Qian */
+/* Date: 2022/12/5    */
+/**********************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <termios.h>
 
@@ -11,43 +19,50 @@
 /**************/
 
 /* Global Variables */
-char** g_map = NULL;
+char **g_map = NULL; // Store the map for drawing
 
-int g_width = -1;
-int g_height = -1;
+int g_width = -1; // map width
+int g_height = -1; // map height
 
-int g_gameover = 0;
+int g_gameover = 0; // mark if the game is over
 
-int g_cursor_x = 0;
-int g_cursor_y = 0;
+int g_cursor_x = 0; // the current x position of cursor
+int g_cursor_y = 0; // the current y position of cursor
 
 #define PEN_DOWN 0
-#define PEN_UP
-int g_pen_state = PEN_DOWN;
+#define PEN_UP 1
+int g_pen_state = PEN_UP; // the pen state
 
-static struct termios old, current;
+static struct termios old, current; // in relation to get input without hit enter
 
 /********************/
 
-void initTermios(int echo) 
+// in relation to get input without hit enter
+void initTermios(int echo)
 {
-  tcgetattr(0, &old);
-  current = old;
-  current.c_lflag &= ~ICANON;
-  if (echo) {
-      current.c_lflag |= ECHO;
-  } else {
-      current.c_lflag &= ~ECHO;
-  }
-  tcsetattr(0, TCSANOW, &current);
+    tcgetattr(0, &old);
+    current = old;
+    current.c_lflag &= ~ICANON;
+    if (echo)
+    {
+        current.c_lflag |= ECHO;
+    }
+    else
+    {
+        current.c_lflag &= ~ECHO;
+    }
+    tcsetattr(0, TCSANOW, &current);
 }
 
-void resetTermios(void) 
+// in relation to get input without hit enter
+void resetTermios(void)
 {
-  tcsetattr(0, TCSANOW, &old);
+    tcsetattr(0, TCSANOW, &old);
 }
 
-char getch(){
+// get input without hit enter
+char getch()
+{
     char ch;
     initTermios(0);
     ch = getchar();
@@ -56,58 +71,113 @@ char getch(){
     return ch;
 }
 
-void init(int width, int height){
-     printf("width: %d, height: %d\n", g_width, g_height);
-
-     g_map = malloc(g_height * sizeof(char*));
-     for(int i=0; i<g_height; i++){
-        g_map[i] = malloc(g_width* sizeof(char));
-     }
-
-     for(int x = 0; x < g_height; x++){
-        for(int y = 0; y < g_width; y++){
-            g_map[y][x] = MAP_BLANK;
+// fill the map with map object "map_object"
+void fill_map(char map_object)
+{
+    for (int y = 0; y < g_height; y++)
+    {
+        for (int x = 0; x < g_width; x++)
+        {
+            g_map[y][x] = map_object;
         }
     }
 }
 
-void update(){
-    switch(getch()){
-        case 'w':
-            g_cursor_y--;
-            if(g_cursor_y < 0)
-                g_cursor_y = 0;
-            break;
-        case 'a':
-            g_cursor_x--;
-            if(g_cursor_x < 0)
-                g_cursor_x = 0;
-            break;
-        case 's':
-            g_cursor_y++;
-            if(g_cursor_y >= g_height)
-                g_cursor_y = g_height - 1;
-            break;
-        case 'd':
-            g_cursor_x++;
-            if(g_cursor_x >= g_width)
-                g_cursor_x = g_width - 1;
-            break;
+// init map
+void init()
+{
+    printf("width: %d, height: %d\n", g_width, g_height);
+
+    g_map = malloc(g_height * sizeof(char *));
+    for (int i = 0; i < g_height; i++)
+    {
+        g_map[i] = malloc(g_width * sizeof(char));
     }
 
-    g_map[g_cursor_y][g_cursor_x] = MAP_X;
+    fill_map(MAP_BLANK);
+
 }
 
-void render(){
+// handle user input
+void update()
+{
+    switch (getch())
+    {
+    case 'w':
+        g_cursor_y--;
+        if (g_cursor_y < 0)
+            g_cursor_y = 0;
+        break;
+    case 'a':
+        g_cursor_x--;
+        if (g_cursor_x < 0)
+            g_cursor_x = 0;
+        break;
+    case 's':
+        g_cursor_y++;
+        if (g_cursor_y >= g_height)
+            g_cursor_y = g_height - 1;
+        break;
+    case 'd':
+        g_cursor_x++;
+        if (g_cursor_x >= g_width)
+            g_cursor_x = g_width - 1;
+        break;
+    case 'p':
+        if (g_pen_state == PEN_DOWN)
+            g_pen_state = PEN_UP;
+        else
+            g_pen_state = PEN_DOWN;
+        break;
+    case 'c':
+        fill_map(MAP_BLANK);
+        break;
+    case 'q':
+        g_gameover = 1;
+        break;
+    }
+
+    if (g_pen_state == PEN_DOWN)
+    {
+        g_map[g_cursor_y][g_cursor_x] = MAP_X;
+    }
+}
+
+// render cursor
+void render_cursor()
+{
+    if (g_pen_state == PEN_DOWN)
+    {
+        printf("\033[1;32mX \033[0m");
+    }
+    else
+    {
+        printf("\033[1;33mX \033[0m");
+    }
+}
+
+// render map
+void render_map()
+{
     printf(" ");
-    for(int x = 0; x < g_width; x++){
+    for (int x = 0; x < g_width; x++)
+    {
         printf("--");
     }
     printf("\n");
 
-    for(int y = 0; y < g_height; y++){
+    for (int y = 0; y < g_height; y++)
+    {
         printf("| ");
-        for(int x = 0; x < g_width; x++){
+        for (int x = 0; x < g_width; x++)
+        {
+
+            if (x == g_cursor_x && y == g_cursor_y)
+            {
+                render_cursor();
+                continue;
+            }
+
             switch (g_map[y][x])
             {
             case MAP_BLANK:
@@ -117,7 +187,7 @@ void render(){
             case MAP_X:
                 printf("X ");
                 break;
-            
+
             default:
                 break;
             }
@@ -126,33 +196,90 @@ void render(){
     }
 
     printf(" ");
-    for(int x = 0; x < g_width; x++){
+    for (int x = 0; x < g_width; x++)
+    {
         printf("--");
     }
     printf("\n");
-
-    printf("------------------\n");
-    printf("x: %d, y:%d\n", g_cursor_x, g_cursor_y);
 }
 
-int main(int argc, char* argv[]){
+// render status
+void render_status()
+{
+    printf("Cursor Position: X: %d, Y:%d\n", g_cursor_x, g_cursor_y);
+    if (g_pen_state == PEN_DOWN)
+    {
+        printf("Pen status: Down\n");
+    }
+    else
+    {
+        printf("Pen status: Up\n");
+    }
+}
 
-    printf("Hi! Welcome to this Etch A Sketch game!\n ");
+// render all the stuff
+void render()
+{
+    system("clear");
 
-    if(argc < 3){
+    render_map();
+
+    printf("\n");
+    render_status();
+}
+
+// print a text character by character with some delay in micro seconds
+void print_with_delay(const char *text, unsigned int millsecs)
+{
+    for (int i = 0; i < strlen(text); i++)
+    {
+        printf("%c", text[i]);
+        fflush(stdout);
+        usleep(millsecs * 1000);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+    // check if the number of parameter is valid
+    if (argc < 3)
+    {
         printf("Please specify the width and the length of the canvas like this:\n ./game [width] [height]\n");
         return 0;
     }
 
     g_width = atoi(argv[1]);
     g_height = atoi(argv[2]);
-    
-    init(g_width, g_height);
 
-    render();
-    while(!g_gameover){
-        update();
-        render();
+    if(g_width == 0 || g_height == 0){
+        printf("Invaild parameter\n");
+        return 0;
     }
 
+    // print some messages
+    char welcome_string[] = "Hi! Welcome to this Etch A Sketch game!\n";
+    print_with_delay(welcome_string, 40);
+
+    char manual_string[] = "Use lower case w, a, s, d to move.\nUse lower case p to switch the pen between up and down. (Yellow \033[1;33mX\033[0m for up, and green \033[1;32mX\033[0m for down)\nUse c to clean the screen.\nUse q to quit the game\n";
+    print_with_delay(manual_string, 10);
+
+    print_with_delay("\n(Press any key to continue...)", 10);
+    getch();
+
+    system("clear");
+
+    // init map
+    init();
+
+    // render the first frame
+    render();
+
+    while (!g_gameover)
+    {
+        // handle user input
+        update();
+        // render all stuff
+        render();
+    }
 }
---
+++ hw01/ReadMe.md
@@ -0,0 +1,39 @@
+# What the code does
+It runs an infinity loop.  
+Inside the loop, the code first gets the user input and renders the canvas.  
+
+# How to use it
+The game was written in C.  
+To compile it, run  
+
+```bash
+make install
+```
+
+Then a file called `game` will be generated.  
+The `game` takes two parameters, which are `[width]` and `[height]`. They are the width and height of the canvas of the game.  
+You can run the game like this:  
+```bash
+./game [width] [height] 
+```
+For example, you can run the game with a canvas has a width of 20 and a height of 10 like this:
+```bash
+./game 20 10
+```
+
+Inside the game, you can:  
+Use lower case w, a, s, d to move.  
+Use lower case p to switch the pen between up and down.(Yellow X for up, and green X for down)  
+Use c to clean the screen.  
+Use q to quit the game.
+
+# Troubleshooting
+If there is no gcc in your system, you can run this to install:
+```bash
+sudo apt-get install gcc
+```
+
+If you do not have the permission to run the game, you can try this:
+```bash
+chmod +x game
+```
